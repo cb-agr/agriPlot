31d0ab918188e2708e727c0dcfc02fd336f2b483
Chris Bach <ceb561@gmail.com>
2021-05-04 22:29:12 -0500

    Changed function name

---
+++ R/encloseVarBoundaries.R
@@ -0,0 +1,15 @@
+# req lib: library(rgeos)
+#elementBoundaries takes fieldBookLLSPDF as input and through a series of functions generates blocks from a user-specific blocking input through a minimumb bounding geometry convex hull algorithm implemented in regeos. 
+
+encloseVarBoundaries <- function(fieldBookLLSPDF,blocking){
+fieldBookLLSPDF <- fieldBookLLSPDF
+myBlocks <- blocking
+fieldBookLLSPDFSplit <- split(fieldBookLLSPDF,fieldBookLLSPDF[[myBlocks]], drop=FALSE)
+fieldBookLLSPDFChull <- lapply(fieldBookLLSPDFSplit, function(x){gConvexHull(x)}) 
+fieldBookLLSPDFChull <- mapply(fieldBookLLSPDFChull, names(fieldBookLLSPDFChull), SIMPLIFY = FALSE,
+                               FUN = function(x,y){x@polygons[[1]]@ID <- y
+                               return(x)})
+fieldBookLLSPDFChull <- do.call(rbind,fieldBookLLSPDFChull)
+fbToChullSPDF <- SpatialPolygonsDataFrame(fieldBookLLSPDFChull, data.frame(blocks = names(fieldBookLLSPDFChull)),match.ID = FALSE)
+return(fbToChullSPDF)
+}
