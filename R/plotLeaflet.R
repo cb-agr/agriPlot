36b2639c629627a49eb540f92436524ac4a01a1e
Chris Bach <ceb561@gmail.com>
2021-05-05 21:41:18 -0500

    Changed function name

--- R/plotLeaflet.R
+++ R/plotLeaflet.R
@@ -1,7 +1,7 @@
 #req lib: library(leaflet)
-#fbLLPlotLeaflet.R takes a fieldbook with long/lat as input (i.e.output from agriPlot.R or agriDemoPlot.R) and displays each respective plot long/lat against satellite imagery using the R leaflet package. The purpose is for quick and easy viewing of plots. 
+#plotLeaflet.R takes a fieldbook with long/lat as input (i.e.output from agriPlot.R or agriDemoPlot.R) and displays each respective plot long/lat against satellite imagery using the R leaflet package. The purpose is for quick and easy viewing of plots. 
 
-fbLLPlotLeaflet <- function(fieldBookLL){
+plotLeaflet <- function(fieldBookLL){
 ll <- as.data.frame(cbind(fieldBookLL$Longitude,fieldBookLL$Latitude))
 names(ll) <- c("Longitude","Latitude") 
 m <- leaflet(ll) %>%
