fdff91b55f593ba7c16592768acf9e6307fdf909
cb-agr <82279715+cb-agr@users.noreply.github.com>
2021-05-11 22:36:09 -0500

    Update findIntersectingPlots.R

--- R/findIntersectingPlots.R
+++ R/findIntersectingPlots.R
@@ -1,13 +1,13 @@
 
 #req lib: library(raster)
-#findIntersectingPlots.R - takes a fieldbook with plots that have a longitude/latitude and a center pivot with longitude/latitude for each span and calculates a distance matrix. 
-#The distance matrix is then filtered by user-defined distance (in meters!!!) from each plot to a pivot point
-#The resulting filtered matrix is then used to output a fieldbook ONLY containing plots that intersect with a pivot point. 
-#The final output of this program can be used to determine where to place filler/border plots that intersect with pivot tracks
+#findIntersectingPlots.R - takes a fieldbook with plots that have a longitude/latitude and pointsOfInterest with longitude/latitude and calculates a distance matrix. 
+#The distance matrix is then filtered by user-defined distance (in meters!!!) from each plot to each pointsOfInterest
+#The resulting filtered matrix is then used to output a fieldbook ONLY containing plots that intersect with pointsOfInterest. 
+#The final output of this program can be used to determine where to place filler/border plots that intersect with any set of points but works well with pivot tracks using pivotTracksLL
 
-findIntersectingPlots <- function(fieldbookLL, pivotTracksLL,distance){
+findIntersectingPlots <- function(fieldbookLL, pointsOfInterest, distance){
 fb <- fieldbookLL
-ptLL <- pivotTracksLL
+ptLL <- pointsOfInterest
 row.names(fb) <- paste("R", 1:nrow(fb), sep="") 
 fbLL <- cbind(fb$Longitude,fb$Latitude)
 message("Calculating distance matrix, this may take a minute or two. FYI, this may produce a very large matrix")
